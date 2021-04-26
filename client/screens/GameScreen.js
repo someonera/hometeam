2959d5bdd2d33e13f4d9367c9be3b877c2ff68ba
Ro Bullingdon <176murrayst@gmail.com>
2021-04-26 17:16:54 +0100

    show this week's game on the screen

--- client/screens/GameScreen.js
+++ client/screens/GameScreen.js
@@ -20,6 +20,8 @@ const GET_GAME = gql`
       id
       startDate
       endDate
+      score
+
     }
   }
 `
@@ -28,44 +30,39 @@ export function GameScreen({route, navigation}) {
 
   const mondaysDate = moment().startOf('isoWeek')
   const sundaysDate = moment().endOf('isoWeek')
+  const [game, setGame] = useState(false)
+  const [listening, setListening] = useState(false)
 
-  const [game, setGame] = React.useState(false) 
-  const {loading, error, data: gameData} = useQuery(GET_GAME, {variables: {endDate: "something"}})  
+  const {loading, error, data: gameData} = useQuery(GET_GAME, {variables: {endDate: sundaysDate}})  
   const [newGame, {loading: newLoading, error: newError, data: newData}] = useMutation(NEW_GAME) 
 
-  useEffect(() => {
-    
-  }, [])
-  
-  
-  if (loading) return <Text>Loading</Text>
-  if (error) return  <Text>Error</Text>
-  
-  if (newLoading) return <Text>Loading</Text>
-  if (newError) return <Text>Error</Text>
-  
-  if (gameData.getGame === null && !game) {
-    const new_game = newGame({variables: {startDate: sundaysDate, endDate: sundaysDate}})
-    console.log(new_game)
-    if (new_game) {
-      setGame(newData.newGame) 
-      console.log("hellothere")
-    } 
-  } else if (!game) {
+  if (gameData && !listening) { 
+    console.log("gameData HERE", gameData)
     setGame(gameData.getGame)
-  }
+    setListening(true)
+  } 
   
+  useEffect(() => {
+    if (gameData && !gameData.getGame) {
+      newGame({variables: {startDate: mondaysDate, endDate: sundaysDate}})
+      .then((newData)=> {
+        console.log("this is new:", newData)
+        setGame(newData.newGame)
+      })
+    }
+  }, [listening])
 
-  console.log("gameData:", gameData)
-  console.log("game:", game)
-  console.log("game", game)
-  console.log("new data", newData)
-  console.log("gameData", gameData)
+  console.log(game)
 
   return (
     <Card containerStyle={{height:"95%"}}>
-      <Text> this is the game screen </Text>
+      <Text> Home VS House </Text>
 
+      <Text>
+        {game.startDate}
+        {game.endDate}
+        score: {game.score}
+      </Text>
     </Card>
   )
 }
